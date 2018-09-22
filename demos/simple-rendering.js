70a11de13d67549c08ae1f4b684065d4874ee0d5
Diana Morozova <inmansuit@gmail.com>
2018-09-22 14:56:26 +0300

    Double cubes
    Background and foreground colors are changed, depth turned on, added a cube rotating in the opposite direction. Failed to replace cube with Blender mesh (doughnut) .

--- demos/simple-rendering.js
+++ demos/simple-rendering.js
@@ -160,7 +160,7 @@ let vertexShader = `
     {
         gl_Position = modelViewProjectionMatrix * vec4(position, 1.0);
         vec3 viewNormal = (modelViewMatrix * vec4(normal, 0.0)).xyz;
-        color = mix(bgColor * 0.8, fgColor, viewNormal.z) + pow(viewNormal.z, 10.0);
+        color = mix(bgColor * 0.5, fgColor, viewNormal.z) + pow(viewNormal.z, 10.0);
     }
 `;
 
@@ -169,13 +169,16 @@ let vertexShader = `
 // **             Application processing               **
 // ******************************************************
 
-let bgColor = vec4.fromValues(1.0, 0.2, 0.3, 1.0);
-let fgColor = vec4.fromValues(1.0, 0.9, 0.5, 1.0);
+//colors are changed
 
+let bgColor = vec4.fromValues(1.0, 0.5, 0.1, 0.0);
+let fgColor = vec4.fromValues(0.3, 0.0, 1.0, 0.1);
+
+//depth turned on
 
 app.clearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3])
-    // .depthTest();
-    .cullBackfaces();
+     .depthTest();
+    //.cullBackfaces();
 
 let program = app.createProgram(vertexShader.trim(), fragmentShader.trim());
 
@@ -193,7 +196,7 @@ let modelViewProjectionMatrix = mat4.create();
 let rotateXMatrix = mat4.create();
 let rotateYMatrix = mat4.create();
 
-let drawCall = app.createDrawCall(program, vertexArray, PicoGL.TRIANGLES)
+let drawCall = app.createDrawCall(program, vertexArray,PicoGL.TRIANGLES)
     .uniform("bgColor", bgColor)
     .uniform("fgColor", fgColor);
 
@@ -201,11 +204,15 @@ let startTime = new Date().getTime() / 1000;
 
 
 
+
+
+
+
 function draw() {
     let time = new Date().getTime() / 1000 - startTime;
 
     mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 100.0);
-    mat4.lookAt(viewMatrix, vec3.fromValues(3, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+    mat4.lookAt(viewMatrix, vec3.fromValues(1, 0, 2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
     mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
 
     mat4.fromXRotation(rotateXMatrix, time * 0.1136);
@@ -221,12 +228,30 @@ function draw() {
     app.clear();
     drawCall.draw();
 
-    // mat4.fromTranslation(modelMatrix, vec3.fromValues(0, 0, 0));
-    // mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
-    // mat4.multiply(modelViewProjectionMatrix, viewProjMatrix, modelMatrix);
-    // drawCall.uniform("modelViewMatrix", modelViewMatrix);
-    // drawCall.uniform("modelViewProjectionMatrix", modelViewProjectionMatrix);
-    // drawCall.draw();
+//new cube rotating in opposite direction
+
+    mat4.perspective(projMatrix, Math.PI / 4, app.width / app.height, 0.1, 100.0);
+    mat4.lookAt(viewMatrix, vec3.fromValues(-1, 0, -2), vec3.fromValues(0, 0, 0), vec3.fromValues(0, -1, 0));
+    mat4.multiply(viewProjMatrix, projMatrix, viewMatrix);
+
+    mat4.fromXRotation(rotateXMatrix, time * 0.1136);
+    mat4.fromYRotation(rotateYMatrix, time * 0.2235);
+    mat4.multiply(modelMatrix, rotateXMatrix, rotateYMatrix);
+
+    mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
+    mat4.multiply(modelViewProjectionMatrix, viewProjMatrix, modelMatrix);
+
+    drawCall.uniform("modelViewMatrix", modelViewMatrix);
+    drawCall.uniform("modelViewProjectionMatrix", modelViewProjectionMatrix);
+
+
+     //mat4.fromTranslation(modelMatrix, vec3.fromValues(-1, 0, 0));
+     //mat4.multiply(modelViewMatrix, viewMatrix, modelMatrix);
+     //mat4.multiply(modelViewProjectionMatrix, viewProjMatrix, modelMatrix);
+     //drawCall.uniform("modelViewMatrix", modelViewMatrix);
+     //drawCall.uniform("modelViewProjectionMatrix", modelViewProjectionMatrix);
+
+     drawCall.draw();
 
     requestAnimationFrame(draw);
 }
